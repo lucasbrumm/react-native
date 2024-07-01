fc2684b7789f9136a56e52a46122ea5b191b06e7
lucas.lugao <lucasbrumfurtado@hotmail.com>
2024-07-01 18:13:45 -0300

    chore: Update package.json with new dependencies and add calendar picker component

--- app-testo/App.tsx
+++ app-testo/App.tsx
@@ -1,15 +1,31 @@
 import { StatusBar } from 'expo-status-bar'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { useEffect, useState } from 'react'
+import { Button, Modal, StyleSheet, Text, View } from 'react-native'
+import CalendarPicker from 'react-native-calendar-picker'
 
 export default function App() {
+  const [openCalendarModal, setOpenCalendarModal] = useState<boolean>(false)
+  const [onChangeDate, setOnChangeDate] = useState<Date>()
+
+  useEffect(() => {
+    console.log(onChangeDate)
+  }, [onChangeDate])
   return (
     <View style={styles.container}>
-      <Text>Clique aqui para marcar</Text>
-      <Button title='Marcar' onPress={() => console.log('Marcar')} />
-      <Text>Visualizar marcações</Text>
-      <Button title='Marcar' onPress={() => console.log('Visualizar')} />
-
       <StatusBar style='auto' />
+      <Text>Abrir Calendário</Text>
+      <Button title='Marcar' onPress={() => setOpenCalendarModal(true)} />
+
+      <Modal
+        visible={openCalendarModal}
+        animationType='slide'
+        style={styles.modalCalendar}
+      >
+        <View style={styles.containerModalCalendar}>
+          <CalendarPicker onDateChange={setOnChangeDate} />
+          <Button title='Fechar' onPress={() => setOpenCalendarModal(false)} />
+        </View>
+      </Modal>
     </View>
   )
 }
@@ -20,5 +36,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  containerModalCalendar: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalCalendar: {
+    // backgroundColor: 'red',
   },
 })
