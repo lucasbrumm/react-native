7ac9d44b8610e9cde949521696d2b4dc32a1bccb
lucas.lugao <lucasbrumfurtado@hotmail.com>
2024-07-01 17:40:54 -0300

    chore: update package.json scripts for expo run commands

--- app-testo/App.tsx
+++ app-testo/App.tsx
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Clique aqui para marcar</Text>
+      <Button title='Marcar' onPress={() => console.log('Marcar')} />
+      <Text>Visualizar marcações</Text>
+      <Button title='Marcar' onPress={() => console.log('Visualizar')} />
+
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +21,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
